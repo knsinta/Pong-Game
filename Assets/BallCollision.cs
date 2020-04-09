3019955ff1372c882040e6c0a1cf1455ead010b7
knsinta <kholisohns16@gmail.com>
2020-04-09 16:57:51 +0700

    Pindah Scene

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    public int speed = 20;
+    public Rigidbody2D ball;
+    public Animator animtr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.velocity = new Vector2(-1,1)*speed;
+        animtr.SetBool("IsMove", true);
+    }
+    // Update is called once per frame
+    void FixedUpdate()//fixed untu benda yg bergerak
+    {
+        if(ball.velocity.x > 0){//bola ke kanan
+            ball.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }else{
+            ball.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name == "WallKanan" || other.collider.name == "WallKiri") {
+            StartCoroutine(jeda());   
+        }
+    }
+
+    IEnumerator jeda(){
+            ball.velocity = Vector2.zero;
+            animtr.SetBool("IsMove", false);
+            ball.GetComponent<Transform>().position = new Vector2(0,0);
+            yield return new WaitForSeconds(1);
+            ball.velocity = new Vector2(-1,1)*speed;
+            animtr.SetBool("IsMove", true);
+    }
+
+    
+}
---
+++ Assets/bola.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bola : MonoBehaviour
+{
+    public int speed = 20; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1,1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
